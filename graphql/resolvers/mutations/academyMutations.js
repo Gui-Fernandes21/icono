3a65e0dcc6a688267d066f2c33d566a635afed14
Guilherme <GuiFernandesBjj@gmail.com@users.noreply.github.com>
2022-02-17 13:59:57 +0100

    tweaks

--- graphql/resolvers/mutations/academyMutations.js
+++ graphql/resolvers/mutations/academyMutations.js
@@ -1,24 +1,33 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const { user, academy, staff } = prisma;
+const { user, academy } = prisma;
 
 const academyMutations = {
   createAcademy: {
     async resolve(parent, { data }) {
       const activeUser = await user.findUnique({ where: { id: +data.userId } });
 
-      if (activeUser.clearance === "OWNER") {
-        const createdAcademy = await academy.create({
-          data: { name: data.name },
+      if (
+        activeUser.clearance === "OWNER" ||
+        activeUser.clearance === "MASTER"
+      ) {
+        return await academy.create({
+          data: {
+            name: data.name,
+            owner_id: +data.userId,
+            staff: {
+              create: {
+                role: "OWNER",
+                user: {
+                  connect: {
+                    id: +data.userId,
+                  },
+                },
+              },
+            },
+          },
         });
-
-        const userStaff = await staff.update({
-          where: { id: activeUser.staff_id },
-          data: { gym_id: createdAcademy.id },
-        });
-
-        return createdAcademy;
       } else {
         throw new Error("You must be an Owner to create an Academy");
       }
